21653e18fa2f2678afedf5411063fbcef8c28070
Omer Aplatony <omerap12@gmail.com>
2023-04-05 13:01:08 +0300

    Add Human object into cell

--- Cell.py
+++ Cell.py
@@ -1,8 +1,15 @@
 import random
+from human import Human
 
 class Cell:
     def __init__(self, row, col, p: float) -> None:
         self.row = row
         self.col = col
-        self.human = 1 if random.random() < p else None
-        self.color = "#ff00ff" if self.human else "#ffffff"
+        self.human = Human() if random.random() < p else None
+        self.color = "#F9F9F9"
+
+    def choose_color(self):
+        if self.human is None:
+            self.color = "#ffffff"
+        colors = ["#0000ff", "#ffff00", "#ff00ff", "#00ffff"]
+        self.color = colors[self.human.S_index - 1]
